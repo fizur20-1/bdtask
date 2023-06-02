e0d7d074982d74c6d708f3c2830645ebfdafaa5d
fizur20-1 <91717919+fizur20-1@users.noreply.github.com>
2023-06-02 20:19:01 +0600

    init

---
+++ task.py
@@ -0,0 +1,40 @@
+# number = int(input())
+# factorial=1
+
+# for i in range(1,number+1):
+#     factorial = factorial*i
+
+# print(factorial)
+   #----------------------------- 
+# number = 9
+
+# for i in range (1,10):
+#     if(i<=5):
+#      for j in range(1,i+1):
+#          print("*", end=" ")
+#     if(i>5):
+#          for j in range(9,i-1,-1):
+#              print("*", end=" ")
+ 
+#     print("\n")
+
+ #----------------------------- 
+
+# sample_list = [11, 45, 8, 11, 23, 45, 23, 45, 89]
+# count_dict = {}
+
+# for item in sample_list:
+#     if item in count_dict:
+#         count_dict[item] += 1
+#     else:
+#         count_dict[item] = 1
+
+# print("Count of each item:", count_dict)
+
+
+# s = "@fahim#$%"
+# a=" "
+# for char in s:
+#     if (char >= "a" and char <= "z") or (char >= "A" and char <= "Z"):
+#         a=a+char
+# print((a))
